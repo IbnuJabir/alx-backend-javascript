799b90938073e3641036bb76b7eae2084547bd82
IbnuJabir <kedirjabir12@gmail.com>
2024-08-12 23:44:11 +0300

    return '' add a condition if type of value is string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (startString === '' || typeof startString !== 'string') return '';
   const newSet = [];
   for (const value of set) {
-    if (value.startsWith(startString)) {
+    if (value.startsWith(startString) && typeof value === 'string') {
       newSet.push(value.slice(startString.length));
     }
   }
